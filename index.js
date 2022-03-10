cb1ece353aab4cb80b10fec4acac08294f80c955
quan <quan12xz@gmail.com>
2022-03-10 16:51:25 +0700

    das

---
+++ index.js
@@ -0,0 +1,16 @@
+const express= require("express")
+const app= express()
+const port= 5000
+const routes= require("./routes/index")
+const cors= require("cors")
+app.use(express.json())
+app.use(cors());
+routes(app);
+app.use('/uploads',express.static("uploads"))
+app.use(express.static(path.join(__dirname,'build')))
+app.get("/*", (req, res)=> {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+app.listen(port, ()=>{
+    console.log("server listen on port "+ port);
+})
